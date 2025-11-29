80c1b5fbb0e79f363307794bd81eda60b85f3700
Louis Chatain <louischatain123@gmail.com>
2025-11-29 20:06:11 +0100

    added routers user and comment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from database.database import Base, engine
-from router import post
+from router import comment, post, user
 
 app = FastAPI()
 
 app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(comment.router)
 
 Base.metadata.create_all(engine)
 

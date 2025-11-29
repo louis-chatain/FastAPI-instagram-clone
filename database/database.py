243239f75104e5da2026cd8377d15bc7f44df3c3
Louis Chatain <louischatain123@gmail.com>
2025-11-29 20:06:46 +0100

    typo. database file now created at the right directory

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
      
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/instance/fastapi-instagram-clone.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database/instance/fastapi-instagram-clone.db"
      
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}

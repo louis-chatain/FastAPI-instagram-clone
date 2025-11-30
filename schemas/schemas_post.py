b3c312334a9023b20b1a53bb275762dfba5799ac
Louis Chatain <louischatain123@gmail.com>
2025-11-30 11:58:59 +0100

    added relationship with user and comment

--- schemas/schemas_post.py
+++ schemas/schemas_post.py
@@ -1,11 +1,31 @@
+from typing import List
 from pydantic import BaseModel
 from datetime import date
+
 
 class PostModel(BaseModel):  # data that we will demand/receive from the user
     image_url: str
     image_url_type: str
     caption: str
 
+#--------------------------------------------------------------------------
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class ConfigDict:
+        from_attributes = True
+
+
+class Comment(BaseModel):
+    id: int
+    text: str
+    username: str
+
+    class ConfigDict:
+        from_attributes = True
 
 class PostDisplay(BaseModel):
     id: int
@@ -13,6 +33,10 @@ class PostDisplay(BaseModel):
     image_url_type: str
     caption: str
     timestamp: date
+    users_id: int
+    users: User
+    comments: List[Comment] = []
 
+    
     class ConfigDict:
         from_attributes = True

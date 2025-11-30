d09793cb7f6704aef58d72d8f281180612734194
Louis Chatain <louischatain123@gmail.com>
2025-11-30 11:58:44 +0100

    added relationship with post

--- schemas/schemas_user.py
+++ schemas/schemas_user.py
@@ -1,3 +1,5 @@
+from datetime import date
+from typing import List
 from pydantic import BaseModel
 
 
@@ -6,11 +8,23 @@ class UserModel(BaseModel):  # data that we will demand/receive from the user
     email: str
     password: str
 
+#--------------------------------------------------------------------------
+
+class Post(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: date
+    users_id: int
+    class ConfigDict:
+        from_attributes = True
 
 class UserDisplay(BaseModel):
     id: int
     username: str
     email: str
+    items: List[Post] = []  #the variable "items" is named so because it is also the name of the relationship in the db model User (MUST be the same!)
 
     class ConfigDict:
         from_attributes = True

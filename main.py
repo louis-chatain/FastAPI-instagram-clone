1d4e83f41470a7be8d309b5143d44f27082cfe95
Louis Chatain <louischatain123@gmail.com>
2025-11-30 20:29:48 +0100

    added an origin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from database.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 from router import authentication, comment, post, user
 
 app = FastAPI()
@@ -10,6 +11,19 @@ app.include_router(user.router)
 app.include_router(comment.router)
 app.include_router(authentication.router)
 
-Base.metadata.create_all(engine)
+origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
-app.mount("/database/files", StaticFiles(directory="database/files"), name="files")  # makes files statically available
+# Access-Control-Allow-Origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount(
+    "/database/files", StaticFiles(directory="database/files"), name="files"
+)  # makes files statically available
+
+Base.metadata.create_all(engine)

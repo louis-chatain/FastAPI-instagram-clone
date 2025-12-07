652fd3f3d67cbacbb1805b78a67f22da2651164a
Louis Chatain <louischatain123@gmail.com>
2025-12-07 23:00:38 +0100

    added cascade on delete

--- database/models.py
+++ database/models.py
@@ -11,7 +11,12 @@ class DbUser(Base):
     email: Column[str] = Column(String)
     hashed_password: Column[str] = Column(String)
 
-    items = relationship('DbPost', back_populates='users')  # variable "items" must be the same in schemas_user
+    items = relationship( # variable "items" must be the same in schemas_user
+        'DbPost',
+        back_populates='users',
+        cascade="all, delete-orphan",
+        passive_deletes=False,
+        ) 
 
 
 class DbPost(Base):
@@ -22,18 +27,25 @@ class DbPost(Base):
     caption: Column[str] = Column(String)
     timestamp: Column[date] = Column(Date)
 
-    users_id: Column[int] = Column(Integer, ForeignKey('user.id'))
+    users_id: Column[int] = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
     users = relationship('DbUser', back_populates='items')   
 
-    comments = relationship('DbComment', back_populates='post')
+    comments = relationship(
+        'DbComment',
+        back_populates='post',
+        cascade="all, delete-orphan",
+        passive_deletes=False,
+        )
 
 
 class DbComment(Base):
     __tablename__: str = "comment"
     id: Column[int] = Column(Integer, primary_key=True, index=True)
     text: Column[str] = Column(String)
-    username: Column[str] = Column(String)
     timestamp: Column[date] = Column(Date)
 
-    post_id: Column[int] = Column(Integer, ForeignKey('post.id'))
+    # user_id: Column[int] = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
+    username: Column[str] = Column(String, ForeignKey('user.username', ondelete='CASCADE'))
+
+    post_id: Column[int] = Column(Integer, ForeignKey('post.id', ondelete='CASCADE'))
     post = relationship("DbPost", back_populates="comments")

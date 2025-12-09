38c41c1d4acaec96179520d899d52ad56f70df85
Louis Chatain <louischatain123@gmail.com>
2025-12-09 22:50:24 +0100

    added test for username input

--- test/test_auth.py
+++ test/test_auth.py
@@ -5,9 +5,15 @@ def test_auth_user(client: TestClient):
     
     response = client.post("/login", data={"username":"louis", "password":"louis"})
     assert response.status_code == 200
+
+def test_auth_user_wrong_username(client: TestClient):
+    client.post("/user/create", json={"username":"louis", "email":"louis", "password":"louis"})
+    
+    response = client.post("/login", data={"username":"chat", "password":"louis"})
+    assert response.status_code == 400 
     
 def test_auth_user_wrong_password(client: TestClient):
     client.post("/user/create", json={"username":"louis", "email":"louis", "password":"louis"})
     
     response = client.post("/login", data={"username":"louis", "password":"patate"})
-    assert response.status_code == 400
+    assert response.status_code == 400 

50e444ec015b3a060f10257678288167b0db0db1
Louis Chatain <louischatain123@gmail.com>
2025-12-13 00:59:22 +0100

    added the update test E2E

--- test/test_post.py
+++ test/test_post.py
@@ -18,6 +18,7 @@ def test_create_post(client: TestClient):
     )
     assert response.status_code == 201
 
+
 def test_read_post(client: TestClient):
     client.post(
         "/user/create", json={"username": "chat", "email": "chat", "password": "patate"}
@@ -30,7 +31,11 @@ def test_read_post(client: TestClient):
 
     response = client.post(
         "/post/create",
-        json={"image_url": "test1", "image_url_type": "test2", "caption": "le caption quoi"},
+        json={
+            "image_url": "test1",
+            "image_url_type": "test2",
+            "caption": "le caption quoi",
+        },
         headers={"Authorization": f"bearer {access_token}"},
     )
     assert response.status_code == 201
@@ -42,10 +47,50 @@ def test_read_post(client: TestClient):
     assert response_value.get("image_url") == "test1"
     assert response_value.get("image_url_type") == "test2"
     assert response_value.get("caption") == "le caption quoi"
-    assert response_value.get("timestamp") == "2025-12-12"
+    assert response_value.get("timestamp") == "2025-12-13"
     user_info = response_value.get("users")
     assert user_info.get("id") == 1
     assert user_info.get("username") == "chat"
     assert user_info.get("email") == "chat"
+    assert response_value.get("comments") == []
 
+
+def test_update_post(client: TestClient):
+    client.post(
+        "/user/create", json={"username": "chat", "email": "chat", "password": "patate"}
+    )
+
+    login_response = client.post(
+        "/login", data={"username": "chat", "password": "patate"}
+    )
+    access_token = login_response.json()["access_token"]
+
+    response = client.post(
+        "/post/create",
+        json={"image_url": "string", "image_url_type": "string", "caption": "string"},
+        headers={"Authorization": f"bearer {access_token}"},
+    )
+    assert response.status_code == 201
+
+    response = client.put(
+        "/post/update",
+        params={"id": 1},
+        json={"image_url": "update1", "image_url_type": "update2", "caption": "update3"},
+        headers={"Authorization": f"bearer {access_token}"}
+    )
+
+    assert response.status_code == 200
+
+    response = client.get("/post/read_all")
+    response_value = response.json()[0]
+    assert response.status_code == 200
+    assert response_value.get("id") == 1
+    assert response_value.get("image_url") == "update1"
+    assert response_value.get("image_url_type") == "update2"
+    assert response_value.get("caption") == "update3"
+    assert response_value.get("timestamp") == "2025-12-13"
+    user_info = response_value.get("users")
+    assert user_info.get("id") == 1
+    assert user_info.get("username") == "chat"
+    assert user_info.get("email") == "chat"
     assert response_value.get("comments") == []

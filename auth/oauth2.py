6ade7bf801c80343752c3405b2fcc37fe7026cb1
Louis Chatain <louischatain123@gmail.com>
2025-12-13 21:16:25 +0100

    updated decrapted function

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -17,9 +17,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
   to_encode = data.copy()
   if expires_delta:
-    expire = datetime.utcnow() + expires_delta
+    expire = datetime.now() + expires_delta
   else:
-    expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.now() + timedelta(minutes=15)
   to_encode.update({"exp": expire})
   encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
@@ -32,7 +32,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
   )
   try:
     payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    username: str = payload.get("username")
+    username = payload.get("username")
     if username is None:
       raise credentials_exception
   except JWTError:

8e73979f221dc847f125b0777d3887a4a8359c27
Louis Chatain <louischatain123@gmail.com>
2025-12-14 19:49:50 +0100

    removed useless lines. alembic now takes are of the databse tables

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from database.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 from router import authentication, comment, post, user
 
@@ -25,5 +24,3 @@ app.add_middleware(
 app.mount(
     "/database/files", StaticFiles(directory="database/files"), name="files"
 )  # makes files statically available
-
-Base.metadata.create_all(engine)
